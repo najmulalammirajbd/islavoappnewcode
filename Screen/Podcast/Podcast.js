25ebe5756f5c2d5ab118938dd6d76a466a065ab2
Najmul Alam Miraj <najmulalammiraj@gmail.com>
2022-08-07 13:35:05 +0600

    m

--- Screen/Podcast/Podcast.js
+++ Screen/Podcast/Podcast.js
@@ -1,12 +1,26 @@
+import React , {useState,useEffect} from 'react';
 import { WebView } from 'react-native-webview';
 import Headir from '../Header/Header';
+import {checkConnected} from '../../NoInternet/functions';
+import NoConnectionScreen from "../../NoInternet/NoConnectionScreen";
 
 export default function Podcast() {
+  const [connectStatus,setConnectStatus] = useState(false)
+
+  checkConnected().then(res=>{
+    setConnectStatus(res)
+  })
+
+  const alertSomething = () =>{
+    alert('alert something')
+  }
     const INJECTED_JAVASCRIPT = `(function() {
         const meta = document.createElement('meta'); meta.setAttribute('content', 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no'); meta.setAttribute('name', 'viewport'); document.getElementsByTagName('head')[0].appendChild(meta);
       })();`;
   return (
-      <>
+    connectStatus?
+      (
+        <>
       <Headir backBtn={true}/>
       <WebView
   source={{ uri: 'https://najmulalammirajbd.github.io/islavopodcast/' }}
@@ -14,5 +28,8 @@ export default function Podcast() {
   injectedJavaScript={INJECTED_JAVASCRIPT}
   onMessage={() => {}}
 /></>
+      ):(
+        <NoConnectionScreen onCheck={checkConnected}/>
+      )
   );
 }
--- Screen/Song/Song.js
+++ Screen/Song/Song.js
@@ -1,12 +1,27 @@
+import React , {useState,useEffect} from 'react';
 import { WebView } from 'react-native-webview';
 import Headir from '../Header/Header';
+import {checkConnected} from '../../NoInternet/functions';
+import NoConnectionScreen from "../../NoInternet/NoConnectionScreen";
 
 export default function Song() {
+  const [connectStatus,setConnectStatus] = useState(false)
+
+  checkConnected().then(res=>{
+    setConnectStatus(res)
+  })
+
+  const alertSomething = () =>{
+    alert('alert something')
+  }
+
     const INJECTED_JAVASCRIPT = `(function() {
         const meta = document.createElement('meta'); meta.setAttribute('content', 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no'); meta.setAttribute('name', 'viewport'); document.getElementsByTagName('head')[0].appendChild(meta);
       })();`;
   return (
-      <>
+    connectStatus?
+      (
+        <>
       <Headir backBtn={true}/>
       <WebView
   source={{ uri: 'https://najmulalammirajbd.github.io/islavosong/' }}
@@ -15,5 +30,8 @@ export default function Song() {
   onMessage={() => {}}
 />
       </>
+      ):(
+        <NoConnectionScreen onCheck={checkConnected}/>
+      )
   );
 }
